8564c8125ecf504bc1d7827c62800c77ef77d1c7
Rusheb Shah <rusheb@apolloresearch.ai>
2024-09-07 18:31:01 +0100

    Strip out all inspect-related code

--- README.md
+++ README.md
@@ -3,15 +3,13 @@ set up
 ```bash
 git clone https://github.com/adrianlyjak/inspect-ai-48-repro
 cd inspect-ai-48-repro
-python3.11 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
+git checkout rs-spike-01
 ```
 
 execute
 
 ```bash
-inspect eval app/evals/eval_foo.py --model openai/gpt-3.5-turbo
+python app/evals/eval_foo.py
 ```
 
 bang!
@@ -22,4 +20,3 @@ bang!
     from app.prompts.foo import GREET
 ModuleNotFoundError: No module named 'app'
 ```
-
--- app/evals/eval_foo.py
+++ app/evals/eval_foo.py
@@ -1,25 +1,9 @@
-from inspect_ai import Task, task
-from inspect_ai.solver import Generate, Solver, TaskState, solver
-from inspect_ai.dataset import MemoryDataset, Sample
 from app.prompts.foo import GREET
 
 
-@solver
-def greet_solver() -> Solver:
-
-    async def solve(state: TaskState, generate: Generate) -> TaskState:
-        prompt = state.user_prompt
-        prompt.text = GREET.format(greeting = state.input)
-        return state
-
-    return solve
+def greet():
+    return GREET.format(greeting="hello")
 
 
-
-@task
-def eval_foo() -> Task:
-    return Task(
-        name="foo",
-        dataset=MemoryDataset(samples=[Sample(input="hello", target="hello world!")]),
-        plan=[greet_solver()],
-    )
+if __name__ == "__main__":
+    print(greet())
